ccc5b7f484d9b0a299ceec112cf770ef84c14c80
Jefferson Vantuir <jefferson.behling@gmail.com>
2018-05-20 20:05:32 -0300

    Criação de models

---
+++ src/java/Model/Cities.java
@@ -0,0 +1,31 @@
+package Model;
+
+public class Cities {
+    private int id;
+    private String name;
+    private int stateId;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getStateId() {
+        return stateId;
+    }
+
+    public void setStateId(int stateId) {
+        this.stateId = stateId;
+    }
+}
---
+++ src/java/Model/States.java
@@ -0,0 +1,31 @@
+package Model;
+
+public class States {
+    private int id;
+    private String name;
+    private String uf;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getUf() {
+        return uf;
+    }
+
+    public void setUf(String uf) {
+        this.uf = uf;
+    }
+}
