dc23d7ad2c2bc15e07d964a551fa0f14a01cd860
Ricardo Hoffmann <ricardohoffmann2@gmail.com>
2018-06-09 13:55:20 -0300

    Criação CategoryDAO

---
+++ src/java/DAO/CategoryDAO.java
@@ -0,0 +1,132 @@
+
+package DAO;
+
+import Model.Category;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author ricardo
+ */
+public class CategoryDAO {
+    private Connection conn;
+    
+    public CategoryDAO()
+    {
+        try {
+            this.conn = ConnectionFactory.getConnection();
+
+        } catch (Exception e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+    }
+    
+    public List list()
+    {
+       try {
+            PreparedStatement pstmt = conn.prepareStatement("SELECT * FROM categories");
+
+            ResultSet rs = pstmt.executeQuery();
+
+            List<Category> listCategories = new ArrayList<Category>();
+
+            while (rs.next()) {
+                Category client = new Category();
+                client.setId(rs.getInt("id"));
+                client.setName(rs.getString("name"));
+                client.setDescription(rs.getString("description"));
+
+                listCategories.add(client);
+            }
+
+            pstmt.close();
+            rs.close();
+
+            return listCategories;
+
+        } catch (SQLException e) {
+            System.out.println("Error: " + e.getMessage());
+            return null;
+        }
+    }
+    
+    public boolean insert(Category category)
+    {
+        try {
+            PreparedStatement pstmt = conn.prepareStatement("INSERT INTO categories (id, name, description) VALUES (?, ?, ?)");
+
+            pstmt.setInt(1, category.getId());
+            pstmt.setString(2, category.getName());
+            pstmt.setString(3, category.getDescription());
+            pstmt.executeUpdate();
+            pstmt.close();
+            return true;
+        } catch (SQLException e) {
+           e.printStackTrace();
+            return false;
+        }
+    }
+    
+    public boolean update(Category category)
+    {
+        try {
+            PreparedStatement pstmt = conn.prepareStatement("UPDATE categories SET name = ?, description = ? WHERE id = ?");
+
+            pstmt.setString(1, category.getName());
+            pstmt.setString(2, category.getDescription());
+            pstmt.setInt(3, category.getId());
+            pstmt.executeUpdate();
+            pstmt.close();
+            return true;
+        } catch (SQLException e) {
+           e.printStackTrace();
+            return false;
+        }
+    }
+    
+    public Category view(int id)
+    {
+       try {
+            PreparedStatement pstmt = conn.prepareStatement("SELECT * FROM categories WHERE id = ?");
+            pstmt.setInt(1, id);
+            ResultSet rs = pstmt.executeQuery();
+
+            Category category = new Category();
+
+            while (rs.next()) {
+                category.setId(rs.getInt("id"));
+                category.setName(rs.getString("name"));
+                category.setDescription(rs.getString("description"));
+            }
+
+            pstmt.close();
+            rs.close();
+
+            return category;
+
+        } catch (SQLException e) {
+            System.out.println("Error: " + e.getMessage());
+            return null;
+        }
+    }
+    
+    public boolean delete(Category categories)
+    {
+        try {
+            PreparedStatement pstmt = conn.prepareStatement("DELETE FROM categories WHERE id = ?");
+
+            pstmt.setInt(1, categories.getId());
+            pstmt.executeUpdate();
+            pstmt.close();
+            return true;
+        } catch (SQLException e) {
+           e.printStackTrace();
+            return false;
+        }
+    }
+}
