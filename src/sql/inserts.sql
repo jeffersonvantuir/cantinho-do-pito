25f0468304e21fac194edc0499943a1152b28242
Jefferson Vantuir <jefferson.behling@gmail.com>
2018-06-10 11:27:37 -0300

    SQL de insertes de States e Cities

---
+++ src/sql/inserts.sql
@@ -0,0 +1,31 @@
+/*
+* STATES INSERTS 
+*/
+
+INSERT INTO states (id, name, uf) VALUES (1, 'Acre', 'AC');
+INSERT INTO states (id, name, uf) VALUES (2, 'Alagoas', 'AL');
+INSERT INTO states (id, name, uf) VALUES (3, 'Amapá', 'AP');
+INSERT INTO states (id, name, uf) VALUES (4, 'Amazonas', 'AM');
+INSERT INTO states (id, name, uf) VALUES (5, 'Bahia', 'BA');
+INSERT INTO states (id, name, uf) VALUES (6, 'Ceará', 'CE');
+INSERT INTO states (id, name, uf) VALUES (7, 'Distrito Federal', 'DF');
+INSERT INTO states (id, name, uf) VALUES (8, 'Espírito Santo', 'ES');
+INSERT INTO states (id, name, uf) VALUES (9, 'Goiás', 'GO');
+INSERT INTO states (id, name, uf) VALUES (10, 'Maranhão', 'MA');
+INSERT INTO states (id, name, uf) VALUES (11, 'Mato Grosso do Sul', 'MS');
+INSERT INTO states (id, name, uf) VALUES (12, 'Mato Grosso', 'MT');
+INSERT INTO states (id, name, uf) VALUES (13, 'Minas Gerais', 'MG');
+INSERT INTO states (id, name, uf) VALUES (14, 'Paraná', 'PR');
+INSERT INTO states (id, name, uf) VALUES (15, 'Paraíba', 'PB');
+INSERT INTO states (id, name, uf) VALUES (16, 'Pará', 'PA');
+INSERT INTO states (id, name, uf) VALUES (17, 'Pernambuco', 'PE');
+INSERT INTO states (id, name, uf) VALUES (18, 'Piauí', 'PI');
+INSERT INTO states (id, name, uf) VALUES (19, 'Rio de Janeiro', 'RJ');
+INSERT INTO states (id, name, uf) VALUES (20, 'Rio Grande do Norte', 'RN');
+INSERT INTO states (id, name, uf) VALUES (21, 'Rio Grande do Sul', 'RS');
+INSERT INTO states (id, name, uf) VALUES (22, 'Rondônia', 'RO');
+INSERT INTO states (id, name, uf) VALUES (23, 'Roraima', 'RR');
+INSERT INTO states (id, name, uf) VALUES (24, 'Santa Catarina', 'SC');
+INSERT INTO states (id, name, uf) VALUES (25, 'Sergipe', 'SE');
+INSERT INTO states (id, name, uf) VALUES (26, 'São Paulo', 'SP');
+INSERT INTO states (id, name, uf) VALUES (27, 'Tocantins', 'TO');
